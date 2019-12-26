81a0af79a7384fb56651074aaf34e0d179cdbd20
otu165 <micheu35@gmail.com>
2019-12-26 15:39:42 +0900

    SignInActivity 생성

--- app/src/main/java/com/example/internz/SignInActivity.kt
+++ app/src/main/java/com/example/internz/SignInActivity.kt
@@ -1,13 +1,61 @@
 package com.example.internz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+    private var backKeyPressedTime : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        
+
+        signInFunction();
+    }
+
+    private fun signInFunction() {
+        //로그인
+        btnSignInLogIn.setOnClickListener {
+            val email = edtSignInEmail.text.toString()
+            val pwd = edtSignInPwd.text.toString()
+
+            //로그인 요청 불가
+            if(email.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력하세요.", Toast.LENGTH_SHORT).show()
+                edtSignInEmail.requestFocus()
+                return@setOnClickListener
+            }
+            else if(pwd.isEmpty()) {
+                Toast.makeText(this, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+                edtSignInPwd.requestFocus()
+                return@setOnClickListener
+            }
+
+            //TODO! 로그인 요청
+        }
+
+        //회원가입
+        txtSignInSignUp.setOnClickListener{
+            val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    //뒤로가기 2번 종료
+    override fun onBackPressed() {
+        val toast = Toast.makeText(this, "\'뒤로\'버튼을 한 번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT)
+
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis()
+            toast.show()
+        }
+        else if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
+            toast.cancel()
+            finish()
+        }
     }
 }
