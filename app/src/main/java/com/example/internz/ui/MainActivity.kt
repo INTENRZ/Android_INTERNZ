7b94bc1722227b5f507f66af17c25991798252c1
chooeeh <chchgml10@gmail.com>
2019-12-28 00:36:14 +0900

    chohee_프로필 뷰 완성

--- app/src/main/java/com/example/internz/ui/MainActivity.kt
+++ app/src/main/java/com/example/internz/ui/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.internz.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.FragmentManager
 import com.example.internz.R
 import com.example.internz.feature.notification.NotificationFragment
 import com.example.internz.ui.Story.StoryFragment
 import com.example.internz.ui.home.HomeFragment
-import com.example.internz.ui.notice.NoticeFragment
-import com.example.internz.ui.profile.ProfileFragment
+import com.example.internz.ui.profile.TimelineFragment
 import kotlinx.android.synthetic.main.activity_home.*
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         val fragmentHome = HomeFragment()
         val fragmentNotification = NotificationFragment()
         val fragmentStory = StoryFragment()
-        val fragmentProfile = ProfileFragment()
+        val fragmentProfile = TimelineFragment()
 
         /* 프래그먼트 교체 관련 */
         val fragmentManager = supportFragmentManager
---
+++ app/src/main/java/com/example/internz/ui/profile/TimelineAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.internz.ui.profile
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.internz.R
+import com.example.internz.data.profile.ProfileTimelineData
+
+class TimelineAdapter(private val context : Context) : RecyclerView.Adapter<TimelineVeiwHolder>() {
+
+    var data = listOf<ProfileTimelineData>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimelineVeiwHolder {
+        val itemView = LayoutInflater.from(context).inflate(R.layout.item_profile_timeline, parent, false)
+        return TimelineVeiwHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: TimelineVeiwHolder, position: Int) {
+        holder.bind(data[position])
+
+        /* 세로 바 없애기 */
+        // 아이템 갯수가 1개일 때
+        if(itemCount == 1){
+            holder.timelineIconSetting()
+        }
+        // 마지막 아이템일 때
+        if(position == itemCount){
+            holder.timelineIconSetting()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
