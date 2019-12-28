602d30224fbb36c52db06630e916eb38e9a38416
Taek <01077275247a@gmail.com>
2019-12-29 04:28:31 +0900

    hyuntaek_ 로그인 fix

--- app/src/main/java/com/example/internz/ui/story/DetailStoryActivity.kt
+++ app/src/main/java/com/example/internz/ui/story/DetailStoryActivity.kt
@@ -1,16 +1,28 @@
 package com.example.internz.ui.story
 
 
+import StoryFragment
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.example.internz.R
 
 
 
 class DetailStoryActivity : AppCompatActivity() {
+
+    //val backicon : ImageView = findViewById(R.id.storyBackImg)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_story)
+
+//        backicon.setOnClickListener {
+//            val intent = Intent(this , StoryFragment::class.java)
+//            startActivity(intent)
+//        }
+
     }
 }
 
