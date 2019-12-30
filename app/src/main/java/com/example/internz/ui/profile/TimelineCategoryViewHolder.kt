f9f95a94c88860948cb5301372e6497403302216
chooeeh <chchgml10@gmail.com>
2019-12-30 19:55:31 +0900

    네비게이션 바 드디어 아이콘 크기랑 색깔 설정함.. 힘들었다

--- app/src/main/java/com/example/internz/ui/profile/TimelineCategoryViewHolder.kt
+++ app/src/main/java/com/example/internz/ui/profile/TimelineCategoryViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.internz.ui.profile
 
 import android.graphics.Color
+import android.util.Log
 import android.view.View
 import android.widget.CheckedTextView
 import android.widget.TextView
@@ -12,30 +13,22 @@ import com.example.internz.feature.jobselect.SelectHelper
 class TimelineCategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val category: CheckedTextView = itemView.findViewById(R.id.checktxt_filter)
     val adapter: TimelineCategoryAdapter = TimelineCategoryAdapter(itemView.context)
-
+    var categoryCheckedIndex = arrayOfNulls<Boolean>(6)
     fun bind(data: TimelineCategoryData){
         category.text = data.category
     }
 
     fun changeColor(){
         itemView.setOnClickListener {
-            // checkedTextView 토글 시키기
-            //itemView.category.isChecked = false
-            //adapter.isAllChecked = false
             category.toggle()
-
-
             // 글자 색도 바꾸기
             if(category.isChecked == true){
                 //TODO(하나만 노란색이 되게 해야함)
-
                 category.setTextColor(Color.parseColor("#ffc200"))
             }else{
                 category.setTextColor(Color.parseColor("#212529"))
             }
 
         }
-
     }
-    
 }
