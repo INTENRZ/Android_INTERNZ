df7c083ead4cad7fd9b87ec2935c59c671f55e4f
otu165 <micheu35@gmail.com>
2020-01-01 10:32:22 +0900

    홈 서버 통신 구현

--- app/src/main/java/com/example/internz/feature/calendar/CalendarActivity.kt
+++ app/src/main/java/com/example/internz/feature/calendar/CalendarActivity.kt
@@ -1,13 +1,11 @@
 package com.example.internz.feature.calendar
 
-import android.graphics.Insets.add
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.applikeysolutions.cosmocalendar.selection.RangeSelectionManager
 import com.applikeysolutions.cosmocalendar.selection.SingleSelectionManager
 import com.applikeysolutions.cosmocalendar.utils.SelectionType
 import com.example.internz.R
@@ -16,8 +14,6 @@ import com.example.internz.common.enqueue
 import kotlinx.android.synthetic.main.activity_calendar.*
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.util.*
-import kotlin.collections.HashSet
 
 
 class CalendarActivity : AppCompatActivity() {
@@ -36,6 +32,42 @@ class CalendarActivity : AppCompatActivity() {
     }
 
     private fun calendarFunction() {
+        /*
+        첫 번째 달력
+        //Calendar Setting
+        calendarView = findViewById(R.id.Calendar)
+        var events = listOf<EventDay>()
+
+        //image setting
+        calendarView.setForwardButtonImage(resources.getDrawable(R.drawable.notice_calendar_next_ic))
+        calendarView.setPreviousButtonImage(resources.getDrawable(R.drawable.notice_calendar_before_ic))
+
+        //add one events
+        calendar = java.util.Calendar.getInstance()
+        calendar.add(java.util.Calendar.DAY_OF_MONTH, 5) //3일에 이벤트 추가
+
+        //TODO ERROR! 무슨짓을 하던 다음달로 설정됨
+        events += events.plus(EventDay(calendar, R.drawable.basicprofile_img))
+
+        var calendar2: java.util.Calendar = java.util.Calendar.getInstance()
+        calendar2.add(java.util.Calendar.DAY_OF_MONTH, 7) //5일에 이벤트 추가
+        events += events.plus(EventDay(calendar2, R.drawable.profile_img))
+
+        //event setting
+        calendarView.setEvents(events)
+
+        //set click listener for calendar
+        calendarView.setOnDayClickListener { eventDay ->
+            run {
+                //TODO! 서버 통신 후 데이터 가져오기
+                Log.e("TAG", eventDay.calendar.time.date.toString())
+                Log.e("TAG", "한번 더")
+            }
+        }
+         */
+
+
+        //두 번째 달력
         //변수 초기화
         calendarView = findViewById(R.id.Calendar)
 
@@ -50,8 +82,10 @@ class CalendarActivity : AppCompatActivity() {
             val singleSelectionManager = calendarView.selectionManager
 
 
+
             Log.e("TAG", singleSelectionManager.toString())
         }
+
 
         //하단 recycler view 목록 지정
         recyclerView = findViewById(R.id.rvCalendar)
@@ -87,6 +121,11 @@ class CalendarActivity : AppCompatActivity() {
 
         //스크롤 이펙트 제거
         findViewById<RecyclerView>(R.id.rvCalendar).overScrollMode = View.OVER_SCROLL_NEVER
+
+        //calendar -> 공고 이동 imageview click listener
+        imgCalendarToNoti.setOnClickListener {
+            this.finish()
+        }
     }
 }
 
