b7f5418982853b8a108c7bd9268682471bfecf48
otu165 <micheu35@gmail.com>
2020-01-03 05:12:42 +0900

    필터 선택 자동 변경 기능 구현

--- app/src/main/java/com/example/internz/feature/filter/FilterHelper.kt
+++ app/src/main/java/com/example/internz/feature/filter/FilterHelper.kt
@@ -1,6 +1,13 @@
 package com.example.internz.feature.filter
 
+import android.widget.CheckedTextView
+import com.example.internz.data.filter.FilterItem
+
 object FilterHelper {
     var count : Int = 0
-    var filterText : String? = null
+
+    //이전의 버튼
+    var lastButton : CheckedTextView? = null
+    //현재 버튼
+    var currentButton : CheckedTextView? = null
 }
