48f45cb86aeadddbf7d44add34dee1769979c4fa
chooeeh <chchgml10@gmail.com>
2020-01-02 17:10:15 +0900

    chohee_프로필 이미지 글라이드 설정 중

--- app/src/main/java/com/example/internz/feature/homerecomm/HomerecommAdapter.kt
+++ app/src/main/java/com/example/internz/feature/homerecomm/HomerecommAdapter.kt
@@ -2,9 +2,13 @@ package com.example.internz.feature.homerecomm
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.internz.R
 import com.example.internz.data.home.RecommProfileData
 import com.example.internz.ui.profile.main.OtherProfileActivity
@@ -23,10 +27,23 @@ class HomerecommAdapter(private val context : Context) : RecyclerView.Adapter<Ho
     override fun onBindViewHolder(holderMain: HomerecommViewHolder, position: Int) {
         holderMain.bind(data[position])
 
-//        holderMain.itemView.setOnClickListener{
-//            val intent = Intent(context, OtherProfileActivity::class.java)
-//            context.startActivity(intent)
-//        }
+        if(data[position].front_image == "undefined"){
+            // 프로필 이미지가 없는 경우 기본 이미지 적용
+            holderMain.img_thumbNail.setImageDrawable(
+                AppCompatResources.getDrawable(
+                    holderMain.itemView.context,
+                    R.drawable.home_recomm_profile_img
+                )
+            )
+        }else{
+            // 프로필 이미지가 있는 경우 해당 이미지 적용
+            Glide
+                .with(holderMain.itemView.context)
+                .load(data[position].front_image)
+                .apply(RequestOptions.circleCropTransform())
+                .into(holderMain.img_thumbNail)
+//            Log.d("chohee", data[position].front_image)
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/internz/feature/homerecomm/HomerecommViewHolder.kt
+++ app/src/main/java/com/example/internz/feature/homerecomm/HomerecommViewHolder.kt
@@ -5,7 +5,10 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.internz.R
 import com.example.internz.data.home.RecommProfileData
 import com.example.internz.ui.profile.main.OtherProfileActivity
